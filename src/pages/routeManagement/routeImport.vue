8687680cfc32b7dfbace3266c402bf536b491f8a
chenkaizhou <chenkaizhou@17dc.com>
2021-09-08 11:08:28 +0800

    航线导出

--- src/pages/routeManagement/routeImport.vue
+++ src/pages/routeManagement/routeImport.vue
@@ -108,8 +108,9 @@
           cancelButtonText: "取消",
           type: "warning"
         }).then(() => {
+          let value = this.ids.replace("，",",").replace(/\s*/g,"");
           axios.post(this.$service.airlineExcelExport, {
-            ids: this.ids.replace("，",",")
+            ids:    value
           }, {
             responseType: 'arraybuffer'
           }).then((res) => {
@@ -127,7 +128,7 @@
               type: "application/vnd.ms-excel"
             })
             aLink.href = URL.createObjectURL(blob)
-            if ([...new Set(this.ids.replace("，",",").split(","))].filter((x)=>x!=='').length > 1) {
+            if ([...new Set(value.split(","))].filter((x)=>x!=='').length > 1) {
               aLink.setAttribute('download', '航线价格表' + '.zip') // 设置下载文件名称
             } else {
               aLink.setAttribute('download', '航线价格表' + '.xlsx') // 设置下载文件名称
