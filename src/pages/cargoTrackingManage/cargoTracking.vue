ece6e78a0c34e2bf645718050b0205ad009c2563
chenkaizhou <chenkaizhou@17dc.com>
2021-11-26 13:11:42 +0800

    货物追踪优化

--- src/pages/cargoTrackingManage/cargoTracking.vue
+++ src/pages/cargoTrackingManage/cargoTracking.vue
@@ -6,6 +6,7 @@
           <el-date-picker
             v-model="accessDate"
             type="daterange"
+            @change="chooseDate();checkButton=0"
             range-separator="至"
             value-format="yyyy-MM-dd"
             start-placeholder="起始日期"
@@ -14,16 +15,16 @@
         </el-form-item>
         <el-form-item>
           <el-row>
-            <el-button @click="chooseDate(1)" :class="checkButton==1?'buttonDiv':'buttonColor2'">
+            <el-button @click="chooseDate(1);checkButton=1" :class="checkButton==1?'buttonDiv':'buttonColor2'">
               今天
             </el-button>
-            <el-button @click="chooseDate(2)" :class="checkButton==2?'buttonDiv':'buttonColor2'">
+            <el-button @click="chooseDate(2);checkButton=2" :class="checkButton==2?'buttonDiv':'buttonColor2'">
               昨天
             </el-button>
-            <el-button @click="chooseDate(3)" :class="checkButton==3?'buttonDiv':'buttonColor2'">
+            <el-button @click="chooseDate(3);checkButton=3" :class="checkButton==3?'buttonDiv':'buttonColor2'">
               近七天
             </el-button>
-            <el-button @click="chooseDate(4)" :class="checkButton==4?'buttonDiv':'buttonColor2'">
+            <el-button @click="chooseDate(4);checkButton=4" :class="checkButton==4?'buttonDiv':'buttonColor2'">
               近三十天
             </el-button>
           </el-row>
@@ -32,14 +33,14 @@
         <el-form-item label="运单号">
           <el-input style="width: 200px;" size="medium" :maxlength="11" onkeyup="value=value.replace(/[^\d]/g, '')"
                     v-model="awb" clearable
-                    placeholder="请输入运单号">
-            <a slot="prefix" class="el-input__icon el-icon-search" @click="searchClick"></a>
+                    placeholder="请输入运单号" @blur="searchClick(3)">
+            <i slot="prefix" class="el-input__icon el-icon-search"></i>
           </el-input>
         </el-form-item>
         <el-form-item label="航司">
           <el-input style="width: 200px;" size="medium" :maxlength="20" v-model="airCPCode" clearable
-                    placeholder="请输入航司">
-            <a slot="prefix" class="el-input__icon el-icon-search" @click="searchClick"></a>
+                    placeholder="请输入航司" @blur="searchClick(3)">
+            <i slot="prefix" class="el-input__icon el-icon-search"></i>
           </el-input>
         </el-form-item>
 
@@ -54,13 +55,13 @@
       </div>
     </el-form>
     <div class="divleft">
-      <el-button @click="chooseDate" size="medium" :class="checkButton1==7?'buttonDiv':'buttonColor2'">
+      <el-button @click="isSupport();checkButton1=7" size="medium" :class="checkButton1==7?'buttonDiv':'buttonColor2'">
         全部
       </el-button>
-      <el-button @click="chooseDate(5)" size="medium" :class="checkButton1==5?'buttonDiv':'buttonColor2'">
+      <el-button @click="isSupport(1);checkButton1=5" size="medium" :class="checkButton1==5?'buttonDiv':'buttonColor2'">
         已支持
       </el-button>
-      <el-button @click="chooseDate(6)" size="medium" :class="checkButton1==6?'buttonDiv':'buttonColor2'">
+      <el-button @click="isSupport(2);checkButton1=6" size="medium" :class="checkButton1==6?'buttonDiv':'buttonColor2'">
         未支持
       </el-button>
       <el-table
@@ -103,29 +104,29 @@
       </div>
     </div>
     <div class="divright">
-      <el-button @click="searchClick(null,2)" size="medium" :class="checkButton2==8?'buttonDiv':''">
+      <el-button @click="searchClick(2)" size="medium" :class="checkButton2==8?'buttonDiv':''">
         全部
       </el-button>
-      <el-button @click="searchClick(null,1)" size="medium" :class="checkButton2==9?'buttonDiv':''">
+      <el-button @click="searchClick(1)" size="medium" :class="checkButton2==9?'buttonDiv':''">
         成功
       </el-button>
       <el-popover
         placement="right-start"
         width="160"
-        v-model="visible">
+        v-model="visible"
+        trigger="manual"
+      >
         <div>
-          <el-checkbox v-model="failType1" @change="changeErrType('1',failType1)" style="display:block;"
-                       label="不支持该航司"></el-checkbox>
-          <el-checkbox v-model="failType2" @change="changeErrType('2',failType2)" style="display:block;"
-                       label="官网访问失败"></el-checkbox>
-          <el-checkbox v-model="failType3" @change="changeErrType('3',failType3)" style="display:block;"
-                       label="未找到该单号"></el-checkbox>
-          <el-checkbox v-model="failType4" @change="changeErrType('4',failType4)" style="display:block;"
-                       label="未找到该单号"></el-checkbox>
+          <el-checkbox-group
+            v-model="errType"
+            :min="1"
+            >
+          <el-checkbox  v-for="(failType,index) in failTypeList" :label="index+1" :key="index+1"  @change="initDetailSearch()">{{failType}}</el-checkbox>
+          </el-checkbox-group>
         </div>
-        <el-button slot="reference" @click="searchClick(null,0)" size="medium" :class="checkButton2==10?'buttonDiv':''">
+        <el-button slot="reference" @click="searchClick(0)" size="medium" :class="checkButton2==10?'buttonDiv':''">
           失败
-          <i class="el-icon-arrow-down "></i>
+          <i class="el-icon-arrow-down  " @click="visible = !visible"></i>
         </el-button>
       </el-popover>
 
@@ -187,15 +188,17 @@
     data() {
       return {
         //table
-        failType1: false,
-        failType2: false,
-        failType3: false,
-        failType4: false,
+        failTypeList: ['不支持该航司','官网访问失败','未找到该单号','未知错误'],
+        failType1: true,
+        failType2: true,
+        failType3: true,
+        failType4: true,
         checkButton: 0,
-        errType: [],
+        errType: [1, 2, 3, 4],
+        visible: false,
         checkButton1: 0,
         checkButton2: 0,
-        isSuccess: '',
+        isSuccess: null,
         tableData: [],
         detailData: [],
         pageSize: 10,
@@ -222,6 +225,7 @@
           ]
         },
         accessDate: [],
+        accessDay: [],
         support: null,
         labelPosition: 'right',
         dialogTitle: '',
@@ -229,22 +233,26 @@
       }
     },
     mounted() {
-      this.accessDate[0] = this.format(new Date(new Date().toLocaleDateString()).getTime())
-      this.accessDate[1] = this.format(new Date(new Date().toLocaleDateString()).getTime() + 24 * 60 * 60 * 1000 - 1);
+      this.accessDay[0] = this.format(new Date(new Date().toLocaleDateString()).getTime())
+      this.accessDay[1] = this.format(new Date(new Date().toLocaleDateString()).getTime() + 24 * 60 * 60 * 1000 - 1);
+      this.checkButton = 1
+      this.checkButton1 = 7
+      this.checkButton2 = 8
       this.initListSearch()
+      this.initDetailSearch()
     },
     methods: {
       //获取代理列表
-      initListSearch(filter) {
+      initListSearch(isSupport) {
         var params = {
-          fromDate: this.accessDate === '' ? '' : this.accessDate[0] + " 00:00:00",
-          toDate: this.accessDate === '' ? '' : this.accessDate[1] + " 23:59:59",
+          fromDate: this.accessDate.length == 0 ? this.accessDay[0] : this.accessDate[0] + " 00:00:00",
+          toDate: this.accessDate.length == 0 ? this.accessDay[1] : this.accessDate[1] + " 23:59:59",
         }
         this.$http.post(this.$service.trackList, params).then(data => {
           if (data.code == 200) {
-            if (filter === 5) {
+            if (isSupport === 1) {
               data.data.airCompanyDate = data.data.airCompanyDate.filter(item => item.isSupport == 1)
-            } else if (filter === 6) {
+            } else if (isSupport === 2) {
               data.data.airCompanyDate = data.data.airCompanyDate.filter(item => item.isSupport != 1)
             }
             this.tableData = data.data.airCompanyDate
@@ -258,88 +266,85 @@
       },
       initDetailSearch(airCPCode, isSuccess) {
         var params = {
-          fromDate: this.accessDate === '' ? '' : this.accessDate[0] + " 00:00:00",
-          toDate: this.accessDate === '' ? '' : this.accessDate[1] + " 23:59:59",
-          awb: airCPCode == null ? this.awb : null,
-          airCPCode: airCPCode != null ? airCPCode : this.airCPCode,
-          isSuccess: isSuccess == null ? this.support : isSuccess,
+          fromDate: this.accessDate.length == 0 ? this.accessDay[0] : this.accessDate[0] + " 00:00:00",
+          toDate: this.accessDate.length == 0 ? this.accessDay[1] : this.accessDate[1] + " 23:59:59",
+          awb: airCPCode == null ? this.awb == "" ? null : this.awb : null,
+          airCPCode: airCPCode != null ? airCPCode : this.airCPCode == "" ? null : this.airCPCode,
+          isSuccess: isSuccess == null ? this.isSuccess : isSuccess,
           pageNum: this.pageNum,
           pageSize: this.pageSize,
-          errType: this.getParam()
+          errType: this.isSuccess == null || this.isSuccess == 1 ? null : this.getParam()
         }
         this.$http.post(this.$service.trackDetail, params).then(data => {
           if (data.code == 200) {
-            this.total = parseInt(data.data.detailsTotalNum)
+            this.total = params.isSuccess == 0 ? data.data.detFailTimes :
+              params.isSuccess == 1 ? data.data.detSuccTimes
+                : data.data.detailsTotalNum;
             this.detailsTotalNum = data.data.detailsTotalNum
             this.detailsSuccRate = data.data.detailsSuccRate
             this.detailsFailRate = data.data.detailsFailRate
-
-            if (this.failType1) {
-              this.detailData.push(data.data.monitors.filter(item => item.failType == 1));
-            }
-            if (this.failType2) {
-              this.detailData.push(data.data.monitors.filter(item => item.failType == 2));
-            }
-            if (this.failType3) {
-              this.detailData.push(data.data.monitors.filter(item => item.failType == 3));
-            }
-            if (this.failType4) {
-              this.detailData.push(data.data.monitors.filter(item => item.failType == 4));
-            }
-            if (this.detailData.length == 0) {
-              this.detailData = data.data.monitors;
-            }
+            this.detailData = data.data.monitors;
           } else {
             this.$message.error(data.message)
           }
         })
       },
       chooseDate(type) {
+        this.awb = null
+        this.airCPCode = null
         if (type === 1) {
-          this.checkButton = 1;
-          this.accessDate[0] = this.format(new Date(new Date().toLocaleDateString()).getTime())
-          this.accessDate[1] = this.format(new Date(new Date().toLocaleDateString()).getTime() + 24 * 60 * 60 * 1000 - 1);
+          this.accessDay[0] = this.format(new Date(new Date().toLocaleDateString()).getTime())
+          this.accessDay[1] = this.format(new Date(new Date().toLocaleDateString()).getTime() + 24 * 60 * 60 * 1000 - 1);
+          this.checkButton1 = 7
+          this.accessDate = []
         } else if (type === 2) {
-          this.checkButton = 2;
-          this.accessDate[0] = this.format(new Date(new Date().toLocaleDateString()).getTime() - 24 * 60 * 60 * 1000);
-          this.accessDate[1] = this.format(new Date(new Date().toLocaleDateString()).getTime() - 1);
+          this.accessDay[0] = this.format(new Date(new Date().toLocaleDateString()).getTime() - 24 * 60 * 60 * 1000);
+          this.accessDay[1] = this.format(new Date(new Date().toLocaleDateString()).getTime() - 1);
+          this.checkButton1 = 7
+          this.accessDate = []
         } else if (type === 3) {
-          this.checkButton = 3;
-          this.accessDate[0] = this.format(new Date(new Date().toLocaleDateString()).getTime() - 7 * 24 * 60 * 60 * 1000);
-          this.accessDate[1] = this.format(new Date(new Date().toLocaleDateString()).getTime() + 24 * 60 * 60 * 1000 - 1);
+          this.accessDay[0] = this.format(new Date(new Date().toLocaleDateString()).getTime() - 7 * 24 * 60 * 60 * 1000);
+          this.accessDay[1] = this.format(new Date(new Date().toLocaleDateString()).getTime() + 24 * 60 * 60 * 1000 - 1);
+          this.checkButton1 = 7
+          this.accessDate = []
         } else if (type === 4) {
-          this.checkButton = 4;
-          this.accessDate[0] = this.format(new Date(new Date().toLocaleDateString()).getTime() - 30 * 24 * 60 * 60 * 1000);
-          this.accessDate[1] = this.format(new Date(new Date().toLocaleDateString()).getTime() + 24 * 60 * 60 * 1000 - 1);
-        } else if (type === 5) {
-          this.checkButton1 = 5;
-        } else if (type === 6) {
-          this.checkButton1 = 6;
-        } else {
-          this.checkButton1 = 7;
+          this.accessDay[0] = this.format(new Date(new Date().toLocaleDateString()).getTime() - 30 * 24 * 60 * 60 * 1000);
+          this.accessDay[1] = this.format(new Date(new Date().toLocaleDateString()).getTime() + 24 * 60 * 60 * 1000 - 1);
+          this.checkButton1 = 7
+          this.accessDate = []
         }
-        this.initListSearch(type)
+        this.initListSearch()
+        this.initDetailSearch()
+      },
+      isSupport(type) {
+        if (type === 1) {
+          this.initListSearch(type)
+        } else {
+          this.initListSearch(type)
+        }
       },
       //查询
       getParam() {
         var result = '';
         this.errType.forEach(x => result = result + x + ",")
-        var x=result.substring(0, result.length - 1)
-        return x.length==0?"1,2,3,4":x
+        var x = result.substring(0, result.length - 1)
+        return x.length == 0 ? "1,2,3,4" : x
       },
-      searchClick(type, isSuccess) {
+      searchClick(isSuccess) {
+
         if (isSuccess === 1) {
+          this.visible=false
           this.checkButton2 = 9
         } else if (isSuccess === 0) {
           this.checkButton2 = 10
-        } else if (isSuccess === 2) {
+        } else if (isSuccess === 2 || isSuccess === 3) {
+          this.visible=false
           this.checkButton2 = 8
           isSuccess = null
         }
         this.isSuccess = isSuccess
         this.pageSize = 10
         this.pageNum = 1
-        // this.initListSearch(type)
         this.initDetailSearch(null, isSuccess)
       },
       //清空
@@ -355,14 +360,6 @@
         return y + '-' + m + '-' + d + ' ' + h + ':' + mm + ':' + s;
       },
       //清空
-      changeErrType(data, option) {
-        if (option) {
-          this.errType.push(data)
-        } else {
-          var index = this.errType.indexOf(data);
-          this.errType.splice(index, 1)
-        }
-      },
       restClick() {
         this.airCPCode = null
         this.awb = null
@@ -371,7 +368,7 @@
 
         this.pageSize = 10
         this.pageNum = 1
-        this.initListSearch()
+        this.searchClick(0)
       },
       // 页码跳转
       handleCurrent(e) {
@@ -385,8 +382,11 @@
       //操作
       handleClick(row) {
         // this.airCPCode = row.airCPCode
-
-        this.initDetailSearch(row.airCPCode)
+        this.checkButton2 = 8
+        this.awb = null
+        this.airCPCode = row.airCPCode
+        this.isSuccess = null
+        this.initDetailSearch()
       },
       sortByDate1(obj1, obj2) {
         let val1 = obj1.totalNum
