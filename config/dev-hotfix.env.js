6da85ccdd5e43118d7c9716a046dd78683ede39a
KangYe <kangye@17dc.com>
2022-02-11 13:22:05 +0800

    chore: 添加环境变量

--- config/dev-hotfix.env.js
+++ config/dev-hotfix.env.js
@@ -4,6 +4,6 @@ const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  ENV_FLAG: '"dev"',
-  BASE_URL: '"/back/dcErp/faw"',
+  ENV_FLAG: '"hotfix"',
+  BASE_URL: '"/back/dcErpHotfix/faw"',
 })
---
+++ config/prod-hotfix.env.js
@@ -0,0 +1,6 @@
+'use strict'
+module.exports = {
+  NODE_ENV: '"production"',
+  ENV_FLAG: '"hotfix"',
+  BASE_URL: '"/back/dcErpHotfix/faw"',
+}
