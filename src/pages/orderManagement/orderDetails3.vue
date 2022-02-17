8d878bdbe20a7c0a5f352c5b15af05471d3c43db
KangYe <kangye@17dc.com>
2022-02-17 18:02:28 +0800

    fix: 修复账单客户发起对账相关问题3

--- src/pages/orderManagement/orderDetails3.vue
+++ src/pages/orderManagement/orderDetails3.vue
@@ -1661,7 +1661,7 @@ export default {
     // 客户发起对账
     async reconciliationClient(e) {
       try {
-         await this.saveOrder()
+         await this.saveOrder(false)
       }catch (err){
         return 
       }
